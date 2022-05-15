ebd10f30867f1bda999a0ee9084c5519ca02ef25
Fogel Egor <fogel_92@bk.ru>
2022-05-15 13:05:46 +0700

    feature: add editing form

--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,29 @@
-let popup = document.querySelector('.popup');
-let profileEditBtn = document.querySelector('.profile__edit-btn');
-let popupCloseBtn = document.querySelector('.popup__close-btn');
+const popup = document.querySelector('.popup');
+const profileEditBtn = document.querySelector('.profile__edit-btn');
+const popupCloseBtn = document.querySelector('.popup__close-btn');
+const profileName = document.querySelector('.profile__name');
+const profileStatus = document.querySelector('.profile__status');
+const formName = document.querySelector('#profile__name');
+const formStatus = document.querySelector('#profile__status');
+const form = document.querySelector('.form');
 
-
-profileEditBtn.addEventListener('click', function() {
+function openPopup() {
   popup.classList.add('popup_opened');
-})
+}
 
-popupCloseBtn.addEventListener('click', function() {
+function closePopup() {
   popup.classList.remove('popup_opened');
-})
+}
+
+function formSubmitHandler(evt) {
+  evt.preventDefault();
+  profileName.textContent = formName.value;
+  profileStatus.textContent = formStatus.value;
+  closePopup()
+}
+
+profileEditBtn.addEventListener('click', openPopup);
+
+popupCloseBtn.addEventListener('click', closePopup);
+
+form.addEventListener('submit', formSubmitHandler);
