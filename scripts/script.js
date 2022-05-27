834892c8d8f74d78000d889c90a3788cee4f1e3b
Fogel Egor <fogel_92@bk.ru>
2022-05-27 16:47:51 +0700

    feat: make a popup add place

--- scripts/script.js
+++ scripts/script.js
@@ -1,31 +1,47 @@
-const popup = document.querySelector('.popup');
-const profileEditBtn = document.querySelector('.profile__edit-btn');
-const popupCloseBtn = document.querySelector('.popup__close-btn');
+const popupEditProfile = document.querySelector('.popup_edit-ptofile');
+const popupAddPlace = document.querySelector('.popup_add-place');
+const popupEditOpenBtn = document.querySelector('.profile__edit-btn');
+const popupEditCloseBtn = document.querySelector('.popup__close-btn_edit');
 const profileName = document.querySelector('.profile__name');
 const profileStatus = document.querySelector('.profile__status');
 const formName = document.querySelector('#profile__name');
 const formStatus = document.querySelector('#profile__status');
 const form = document.querySelector('.form');
+const likeBtns = document.querySelectorAll('.gallery__like-btn');
+const popupAddOpenBtn = document.querySelector('.profile__add-btn');
+const popupAddCloseBtn = document.querySelector('.popup__close-btn_add');
 
-function openPopup() {
-  popup.classList.add('popup_opened');
+function openPopup(element) {
+  element.classList.add('popup_opened');
   formName.value = profileName.textContent;
   formStatus.value = profileStatus.textContent;
 }
 
-function closePopup() {
-  popup.classList.remove('popup_opened');
+function closePopup(element) {
+  element.classList.remove('popup_opened');
 }
 
 function formSubmitHandler(evt) {
   evt.preventDefault();
   profileName.textContent = formName.value;
   profileStatus.textContent = formStatus.value;
-  closePopup()
+  closePopup(popupEditProfile);
 }
 
-profileEditBtn.addEventListener('click', openPopup);
+popupEditOpenBtn.addEventListener('click', () => {
+  openPopup(popupEditProfile)
+});
 
-popupCloseBtn.addEventListener('click', closePopup);
+popupEditCloseBtn.addEventListener('click', () => {
+  closePopup(popupEditProfile)
+});
+
+popupAddOpenBtn.addEventListener('click', () => {
+  openPopup(popupAddPlace)
+})
+
+popupAddCloseBtn.addEventListener('click', () => {
+  closePopup(popupAddPlace)
+})
 
 form.addEventListener('submit', formSubmitHandler);
