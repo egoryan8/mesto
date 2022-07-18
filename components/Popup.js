9c2a54448200343f7a712ec05cbd937f126bf6a7
Fogel Egor <fogel_92@bk.ru>
2022-07-18 19:25:11 +0700

    feat: add class Popup

---
+++ components/Popup.js
@@ -0,0 +1,29 @@
+export default class Popup {
+  constructor(popupSelector) {
+    this._popup = document.querySelector(popupSelector);
+  }
+
+  open() {
+    this._popup.classList.add('popup_opened');
+    document.addEventListener('keydown', this._handleEscClose);
+  }
+
+  close() {
+    this._popup.classList.remove('popup_opened');
+    document.removeEventListener('keydown', this._handleEscClose);
+  }
+
+  _handleEscClose() {
+    if (evt.key === 'Escape') {
+      this.close();
+    }
+  }
+
+  setEventListeners() {
+    this._popup.addEventListener('mousedown', (evt) => {
+      if (evt.target === evt.currentTarget || evt.target.classList.contains('popup__close-btn')) {
+        this.close();
+      }
+    });
+  }
+}
