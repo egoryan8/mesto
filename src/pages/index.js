1c3b77fcaa38ae1a69859bbf067e1203752a3f78
Fogel Egor <fogel_92@bk.ru>
2022-07-20 14:55:21 +0700

    feat: webpack build

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,6 +5,8 @@ import PopupWithImage from '../components/PopupWithImage.js';
 import Section from '../components/Section.js';
 import UserInfo from '../components/UserInfo.js';
 
+import './index.css';
+
 import {
   initialCards,
   config,
